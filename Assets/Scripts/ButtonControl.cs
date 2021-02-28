ebf74f8e8d9999d712b598560865a3806b94a3cb
para29 <65754533+para29@users.noreply.github.com>
2021-02-28 15:01:25 +0000

    NotePadd

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonControl : MonoBehaviour
+{
+    public GameObject theText;
+    public AudioSource clearSound;
+    public GameObject thePanel;
+
+
+    public void ClearText()
+    {
+        theText.GetComponent<InputField>().text = "";
+        clearSound.Play();
+    }
+
+    public void cancelButton()
+    {
+        thePanel.SetActive(false);
+    }
+
+    public void closeButton()
+    {
+        thePanel.SetActive(true);
+    }
+
+    public void QuitButton()
+    {
+       Application.Quit();
+    }
+
+
+}
---
+++ Assets/Scripts/MoveScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Movetonext());
+    }
+
+    IEnumerator Movetonext()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(1);
+    }
+
+    
+}
